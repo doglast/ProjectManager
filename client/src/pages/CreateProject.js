666a59dadca5102d16db195e04202faba69e03f6
Doglast <des212425@gmail.com>
2024-06-19 20:04:53 -0300

    criação de telas com comunicação com back-end

---
+++ client/src/pages/CreateProject.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+export default function CreateProject() {
+  const initialValues =
+  {
+    project_name:"",
+    project_description:"",
+    project_start_date:"",
+  }
+
+  const onSubmit = (data) =>{
+    axios.post("http://localhost:3001/projects", data).then((response) =>{
+      console.log("Posted"); // todo modal de sucesso e redirect
+    });
+  }
+
+  const validationSchema = Yup.object.shape({
+    project_name: Yup.string().required(),
+    project_description: Yup.string(),
+    project_start_date: Yup.date().required()
+  })
+  
+
+  return (
+    <div className='createProjectContainer' >
+      <Formik 
+        initialValues={initialValues} 
+        onSubmit={onSubmit} 
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <label>Nome do projeto</label>
+          <ErrorMessage name="project_name" component="span" />
+          <Field 
+            autocomplete="off"
+            id="inputCreateProject" 
+            name="project_name" 
+            placeholder="Nome do projeto"
+          />
+          <label>Descrição do projeto</label>
+          <ErrorMessage name="project_description" component="span" />
+          <Field 
+            autocomplete="off"
+            id="inputCreateProject" 
+            name="project_description" 
+            placeholder="Descrição do projeto"
+          />
+          <label>Data de início do projeto</label>
+          <ErrorMessage name="project_start_date" component="span" />
+          <Field 
+            autocomplete="off"
+            id="inputCreateProject" 
+            name="project_start_date" 
+            placeholder="DD/MM/AAAA"
+          />
+
+          <button type='submit'>Criar projeto</button>
+
+        </Form>
+      </Formik>
+    </div>
+  )
+}
